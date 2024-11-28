a8c5d5743e4ead824f48c1a443e0c0d56397ebcd
Kevin Maykon Batalha Santos <kevinoukv123@gmail.com>
2024-11-28 20:11:08 -0300

    Remove class Task

--- Task.py
+++
@@ -1,9 +0,0 @@
-class Task():
-    def __init__(self, name):
-        self.name = name
-        self.completed = False
-        #self.sub_tasks = []
-
-    """def addSuTask(new_sub_task):
-        self.sub_tasks.append(new_sub_task)**/"""
-        
--- Todolist.py
+++ Todolist.py
@@ -2,9 +2,15 @@ class Todolist():
     def __init__(self, name):
         self.name = name
         self.tasks = []
+        self.completed = False
 
-    def addTask(self, new_task):
-        self.tasks.append(new_task)
+    def addTask(self, new_task: str):
+        self.tasks.append(Todolist(new_task))
 
     def removeTask(self, task_id):
         self.tasks.pop(task_id)
+
+    def showSubTasks(self):
+        for item in self.tasks:
+            print(item.name)
+        
