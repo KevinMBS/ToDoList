6cd509ba0a270325c0c6d7c88dbdce22a9f91f8b
Kevin Maykon Batalha Santos <kevinoukv123@gmail.com>
2024-11-29 15:01:27 -0300

    Tasks can now be added

--- main.py
+++ main.py
@@ -2,20 +2,25 @@ BASE_WIDGETS="#ECEBDE"
 BG="#A59D84"
 BTN="#C1BAA1"
 import flet as ft
-
+import todolist as tdl
 
 def main(page: ft.Page):
     page.title="To do App"
     page.theme_mode=ft.ThemeMode.DARK
+    tasks = []
 
     def add_new_task(e):
+        new_tdl = tdl.Todolist(new_task_field.value)
+        #salvar new_tdl no DB
+        tasks.append(ft.Checkbox(label=new_task_field.value))
+        tasks_view.controls = tasks
         new_task_field.value=""
         page.update()
 
     #Creating elements
     new_task_field=ft.TextField(hint_text="What's needs to be done?",border_color=BASE_WIDGETS,expand=True)
     btn_add_task=ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=add_new_task,bgcolor=BTN)
-    tasks_view = ft.Column()
+    tasks_view = ft.Column(scroll=True)
     insertion_fields=ft.Row(controls=[new_task_field,btn_add_task])
     visualization_fields=ft.Row(controls=[tasks_view])
 
